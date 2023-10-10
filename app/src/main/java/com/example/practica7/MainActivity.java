1f00f88ccc9938d7d3d1cb77fddd611258268bc9
dabidgmz <david.gmzherrera28@gmail.com>
2023-10-09 21:36:52 -0600

    muchos mas cambios

--- app/src/main/java/com/example/practica7/MainActivity.java
+++ app/src/main/java/com/example/practica7/MainActivity.java
@@ -1,49 +1,60 @@
 package com.example.practica7;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.example.practica7.models.Celcius;
-import com.example.practica7.models.Kelvin;
-import com.example.practica7.models.Fahrenheit;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.practica7.Models.Celcius;
+import com.example.practica7.Models.Fahreinheit;
+import com.example.practica7.Models.Kelvin;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextValor;
-    private Button buttonConvertir;
-    private TextView textViewResultado;
+    private EditText editTextTemperature;
+    private Button buttonConvert;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextValor = findViewById(R.id.editTextValor);
-        buttonConvertir = findViewById(R.id.buttonConvertir);
-        textViewResultado = findViewById(R.id.textViewResultado);
+        editTextTemperature = findViewById(R.id.editTextTemperature);
+        buttonConvert = findViewById(R.id.buttonConvert);
+        textViewResult = findViewById(R.id.textViewResult);
 
-        buttonConvertir.setOnClickListener(new View.OnClickListener() {
+        buttonConvert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String valorStr = editTextValor.getText().toString();
-                if (!valorStr.isEmpty()) {
-                    double valor = Double.parseDouble(valorStr);
-
-
-                    Celcius celsius = new Celcius(valor);
-                    Kelvin kelvin = new Kelvin(valor);
-                    Fahrenheit fahrenheit = new Fahrenheit(valor);
-
-                    String resultado = "Celsius: " + celsius.convertirACelsius() + "\n"
-                            + "Kelvin: " + kelvin.convertirAKelvin() + "\n"
-                            + "Fahrenheit: " + fahrenheit.convertirAFahrenheit();
-
-                    textViewResultado.setText(resultado);
-                }
+            public void onClick(View view) {
+                convertTemperature();
             }
         });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void convertTemperature() {
+        try {
+            double inputValue = Double.parseDouble(editTextTemperature.getText().toString());
+
+            Celcius celcius = new Celcius();
+            celcius.setValor(inputValue);
+
+            Fahreinheit fahrenheit = new Fahreinheit();
+            fahrenheit.setValor(inputValue);
+
+            Kelvin kelvin = new Kelvin();
+            kelvin.setValor(inputValue);
+
+            fahrenheit = fahrenheit.parse(celcius);
+            kelvin = kelvin.parse(celcius);
+
+            textViewResult.setText("Celsius a Fahrenheit: " + fahrenheit.getValor() +
+                    "\nCelsius a Kelvin: " + kelvin.getValor());
+        } catch (NumberFormatException e) {
+            textViewResult.setText("Invalid input");
+        }
     }
 }
