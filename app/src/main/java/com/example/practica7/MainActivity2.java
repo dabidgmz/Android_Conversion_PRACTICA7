e92bfc2929478163ed102db5113c0c3f584762e2
dabidgmz <david.gmzherrera28@gmail.com>
2023-10-10 09:56:48 -0600

    vete ya .. si no en cuentras motivos para seguir conmigo para que continuar

--- app/src/main/java/com/example/practica7/MainActivity2.java
+++ app/src/main/java/com/example/practica7/MainActivity2.java
@@ -1,8 +1,11 @@
 package com.example.practica7;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        new Handler(Looper.getMainLooper()).postDelayed(() ->{
+            Intent intent=new Intent(MainActivity2.this, MainActivity2.class);
+            startActivity(intent);
+            finish();
+        },3000);
     }
 }
